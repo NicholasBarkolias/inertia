f78b0486abe2336fb4b3b8eb627f61d46f42b626
Fly Dev <dev@fly.io>
2025-06-23 12:18:31 +0000

    remove esbuild config and configure for vite

---
+++ assets/assets/config/config.exs
@@ -0,0 +1,55 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+import Config
+
+config :inertia_app,
+  ecto_repos: [InertiaApp.Repo],
+  generators: [timestamp_type: :utc_datetime]
+
+# Configures the endpoint
+config :inertia_app, InertiaAppWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [html: InertiaAppWeb.ErrorHTML, json: InertiaAppWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: InertiaApp.PubSub,
+  live_view: [signing_salt: "ZhMwFjNJ"]
+
+# Configure inertia
+config :inertia,
+  endpoint: InertiaAppWeb.Endpoint,
+  static_paths: ["/assets/app.js"],
+  default_version: "1",
+  camelize_props: true,
+  history: [encrypt: false],
+  ssr: false,
+  raise_on_ssr_failure: config_env() != :prod
+
+# Configure tailwind (the version is managed by the tailwind dependency in mix.exs)
+config :tailwind,
+  version: "3.4.3",
+  inertia_app: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/css/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
+# Configures Elixir's Logger
+config :logger, :console, format: "$time $metadata[$level] $message\n"
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
