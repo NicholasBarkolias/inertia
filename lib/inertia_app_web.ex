7e8a17963e178d2b4c983486ea1f68c115aba2a9
Fly Dev <dev@fly.io>
2025-06-23 12:04:26 +0000

    fix indentation and syntax in inertia_app_web.ex

--- lib/inertia_app_web.ex
+++ lib/inertia_app_web.ex
@@ -26,7 +26,7 @@ defmodule InertiaAppWeb do
       # Import common connection and controller functions to use in pipelines
       import Plug.Conn
       import Phoenix.Controller
-        import Inertia.Controller
+      import Inertia.Controller
       import Phoenix.LiveView.Router
     end
   end
@@ -69,8 +69,9 @@ defmodule InertiaAppWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller,
-        import Inertia.Controller
         only: [get_csrf_token: 0, view_module: 1, view_template: 1]
+
+      import Inertia.Controller
 
       # Include general helpers for rendering HTML
       unquote(html_helpers())
