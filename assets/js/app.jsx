0b0ef25cd629a629aa189e4a85de637178b7a255
Fly Dev <dev@fly.io>
2025-06-23 13:12:06 +0000

    fix Phoenix imports for Inertia app

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -12,27 +12,3 @@ createInertiaApp({
     root.render(<App {...props} />);
   },
 });
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html";
-// Establish Phoenix Socket and LiveView configuration.
-import { Socket } from "phoenix";
-import { LiveSocket } from "phoenix_live_view";
-import topbar from "../vendor/topbar";
-
-const csrfToken = document
-  .querySelector("meta[name='csrf-token']")
-  .getAttribute("content");
-const liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
-  params: { _csrf_token: csrfToken },
-});
-
-// Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-
-// connect if there are any LiveViews on the page
-liveSocket.connect();
-
-// expose liveSocket on window for web console debug logs and latency simulation:
-window.liveSocket = liveSocket;
